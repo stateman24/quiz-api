be5e5fc59e7f274abbc321c3939a7d254a0e71cd
Daniel Ajibewa <ajibewadannyboi@gmail.com>
2025-02-07 12:04:36 +0100

    provided a consice validation message

--- src/schemas/question.validation.schema.ts
+++ src/schemas/question.validation.schema.ts
@@ -1,9 +1,9 @@
 import { string, z } from "zod"
 
 export const questionValidationSchema = z.object({
-    question: z.string().min(10, "Question must be more than 10 characters"),
-    options: z.array(string()).min(3, "At least 3 options are required"),
-    correctOption: z.string().min(1, "Correct option is required"),
+    question: z.string().min(10, "Question must be at least 10 characters long"),
+    options: z.array(string()).min(4, "At least 4 options are required"),
+    correctOption: z.string(),
 })
 
 export type IQuestionData = z.infer<typeof questionValidationSchema>;
--- src/schemas/quiz.validation.schema.ts
+++ src/schemas/quiz.validation.schema.ts
@@ -2,7 +2,7 @@ import { z } from "zod"
 import { questionValidationSchema } from "./question.validation.schema"
 
 export const createQuizValidationSchema = z.object({
-    title: z.string().min(5, "Question must be at least 5 charcters long"),
+    title: z.string().min(20, "Title must be at least 20 charcters long"),
     description: z.string().min(10, "Description must be at least 10 charcters long"),
     questions: z.array(questionValidationSchema),
     difficulty: z.string(),
