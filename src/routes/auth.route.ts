6ff9d8dd69c94a9b0da88de6fade6378e948028a
Bello Shehu <belloshehu1@gmail.com>
2025-02-08 21:35:30 +0100

    Fix auth route

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -9,7 +9,7 @@ import {
 import authMiddleware from "../middlewares/auth.middleware";
 
 class AuthRoute implements Routes {
-	public path = "";
+	public path = "/auth";
 	public router = Router();
 	private authController = new AuthController();
 
